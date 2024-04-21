7b8d70f2bd511bdd98b23e64f0ec9e0cd4b51568
Paul Peñaherrera <paul.penaherrera@epn.edu.ec>
2024-04-20 20:23:52 -0500

    dataframe deudas

---
+++ src/conect_database.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import psycopg2
+
+def read_data():
+    try:
+        conn = psycopg2.connect(
+            host='localhost',
+            user='postgres',
+            password='root',
+            database='arriendos',
+            port='5432'
+        )  # type: ignore
+        print('Conexión exitosa')
+        cur = conn.cursor()
+        sql = '''select u.nombre, u.fecha_ingreso, u.precio_pactado, d.departamento, d.id_departamento, 
+                p.mes_pago, p.year_pago, p.monto_pago, p.fecha_pago from usuarios u
+                join departamentos d on d.id_usuario = u.id_usuario
+                join Pagos p on u.id_usuario = p.id_usuario;'''
+        cur.execute(sql)
+        datos = cur.fetchall()
+        columnas = [descripcion[0] for descripcion in cur.description]
+        cur.close()
+        conn.close()
+        df = pd.DataFrame(datos, columns=columnas)
+    except Exception as ex:
+        print(f'Error: {ex}')
+    else:
+        print('Cerrando sesión')
+        
+    return df
